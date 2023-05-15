e52679d1543425ea36c3dfe1cfe4c954778805e4
Mamy RAZAFIARISON <razah99@gmail.com>
2023-05-15 16:24:44 +0200

    add header and footer for all pages

--- php/footer.php
+++ php/footer.php
@@ -1,4 +1,70 @@
+<footer>
+    <hr class="border border-dark border-2 opacity-50 w-75 ms-auto me-auto"><br>
+
+    <h3 class="text-center" >Nous contacter</h3><br>
+    <h5 class="text-center" >Par Téléphone:</h5>
+    <p class="text-center" >06 06 00 00 00</p><br>
+    <h5 class="text-center" >Par email:</h5>
+    <p class="text-center" >contact@le-quai-antique.test</p><br>
+    <h5 class="text-center" >Adresse:</h5>
+    <p class="text-center" >Rue de la pommeraie<br>73000 Chambery</p>
+
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col" colspan="2" class="text-center p-5">Nos horaires d'ouverture:</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Lundi</td>
+                <td class="text-end">12:00 - 14:00<br>19:00 - 22:00</td>
+            </tr>
+            <tr>
+                <td>Mardi</td>
+                <td class="text-end">12:00 - 14:00<br>19:00 - 22:00</td>
+            </tr>
+            <tr>
+                <td>Mercredi</td>
+                <td class="text-end">Fermé</td>
+            </tr>
+            <tr>
+                <td>Jeudi</td>
+                <td class="text-end">12:00 - 14:00<br>19:00 - 22:00</td>
+            </tr>
+            <tr>
+                <td>Vendredi</td>
+                <td class="text-end">12:00 - 14:00<br>19:00 - 22:00</td>
+            </tr>
+            <tr>
+                <td>Samedi</td>
+                <td class="text-end">19:00 - 23:00</td>
+            </tr>
+            <tr>
+                <td>Dimanche</td>
+                <td class="text-end">12:00 - 14:00</td>
+            </tr>
+        </tbody>
+    </table>
+
+    <ul class="nav justify-content-center bg-secondary p-3">
+  <li class="nav-item">
+    <a class="nav-link link-light" href="#">Accueil</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link link-light" href="#">Voir la carte</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link link-light" href="#">Réserver une table</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link link-light" href="#">C.G.V.</a>
+  </li>
+</ul>
+
+</footer>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
+
 </html>
--- php/header.php
+++ php/header.php
@@ -10,30 +10,35 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar w/ text</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarText">
-                <ul class="nav justify-content-center">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Active</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled">Disabled</a>
-                    </li>
-                </ul>
-                <span class="navbar-text">
-                    Navbar text with an inline element
-                </span>
-            </div>
-        </div>
-    </nav>
+<nav class="navbar navbar-expand-lg bg-secondary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Le Quai Antique</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Accueil</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Carte
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Entrées</a></li>
+            <li><a class="dropdown-item" href="#">Plats</a></li>
+            <li><a class="dropdown-item" href="#">Desserts</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Horaires</a>
+        </li>
+      </ul>
+      <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+  <button type="button" class="btn btn-secondary">Connexion</button>
+  <button type="button" class="btn btn-dark">Inscription</button>
+</div>
+    </div>
+  </div>
+</nav>
